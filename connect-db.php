4da19369ee520b21e81365f04a7cd4b89d4c923a
tugu-baterdene <tugu.college@gmail.com>
2025-11-06 15:13:56 -0500

    added connect-db

---
+++ connect-db.php
@@ -0,0 +1,34 @@
+<?php
+
+/** F25, PHP (on GCP, local XAMPP, or CS server) connect to MySQL (on CS server) **/
+$username = 'fnf8eh'; 
+$password = 'PhpSurf@123';
+$host = 'mysql01.cs.virginia.edu';
+$dbname = 'fnf8eh_a';
+$dsn = "mysql:host=$host;dbname=$dbname";
+////////////////////////////////////////////
+
+
+/** connect to the database **/
+try 
+{
+//  $db = new PDO("mysql:host=$hostname;dbname=db-demo", $username, $password);
+   $db = new PDO($dsn, $username, $password);
+   
+   // dispaly a message to let us know that we are connected to the database 
+   // echo "<p>You are connected to the database -- host=$host</p>";
+}
+catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
+{
+   // Call a method from any object, use the object's name followed by -> and then method's name
+   // All exception objects provide a getMessage() method that returns the error message 
+   $error_message = $e->getMessage();        
+   echo "<p>An error occurred while connecting to the database: $error_message </p>";
+}
+catch (Exception $e)       // handle any type of exception
+{
+   $error_message = $e->getMessage();
+   echo "<p>Error message: $error_message </p>";
+}
+
+?>
