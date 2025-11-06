c29e182e62637ece0610951d406ab99b8a1b3990
gracej00sung <rgp3qv@virginia.edu>
2025-11-06 16:04:35 -0500

    changed addUser function

--- sign_up-db.php
+++ sign_up-db.php
@@ -1,14 +1,13 @@
 <?php
-function addUsers($computing_id, $email, $first_name, $last_name, $passwd)
+function addUsers($comp_id, $phone_number, $name, $passwd)
 {
 	global $db;
-	$query = "INSERT INTO sign_up SET computing_id=:computing_id, email=:email, first_name=:first_name, last_name=:last_name, passwd=:passwd";
+	$query = "INSERT INTO sign_up SET comp_id=:comp_id, phone_number=:phone_number, name=:name, passwd=:passwd";
 	try {
     	$statement = $db->prepare($query);
-		$statement->bindValue(':computing_id', $computing_id); 
-		$statement->bindValue(':email', $email); 
-		$statement->bindValue(':first_name', $first_name); 
-		$statement->bindValue(':last_name', $last_name);
+		$statement->bindValue(':comp_id', $comp_id); 
+		$statement->bindValue(':phone_number', $phone_number); 
+		$statement->bindValue(':name', $name); 
 		$statement->bindValue(':passwd', $passwd);
     	$statement->execute();
     	$statement->closeCursor();
