04763d4881de7931532a331b6c6d4d38cb66ee0f
gracej00sung <rgp3qv@virginia.edu>
2025-11-06 16:02:36 -0500

    added user table

--- sign_up.sql
+++ sign_up.sql
@@ -1,6 +1,9 @@
-CREATE TABLE user(computing_id VARCHAR(10) NOT NULL, 
-                      email VARCHAR(40) NOT NULL,
-                      first_name VARCHAR(30), 
-                      last_name VARCHAR(50),
-                      passwd VARCHAR(255) NOT NULL,
-                      PRIMARY KEY (computing_id));
+CREATE TABLE users (
+    comp_id VARCHAR(10) NOT NULL UNIQUE,
+    name VARCHAR(50),
+    passwd VARCHAR(25),
+    phone_number VARCHAR(15),
+    year INT,
+    major VARCHAR(50),
+    bio VARCHAR(300),
+    PRIMARY KEY (comp_id));
