4001b5d5985c6f60b33fa0ff38d07ff64347bd2f
gracej00sung <rgp3qv@virginia.edu>
2025-11-06 22:29:41 -0500

    updated addUsers and added getAllUsers

--- sign_up-db.php
+++ sign_up-db.php
@@ -1,26 +1,44 @@
 <?php
 function addUsers($comp_id, $stu_name, $phone_number, $passwd, $school_year, $major, $bio)
 {
-	global $db;
-	$query = "INSERT INTO sign_up SET comp_id=:comp_id, stu_name=:stu_name, phone_number=:phone_number, passwd=:passwd, school_year=:school_year, major=:major, bio=:bio";
+	global $db; 
+	$query = "INSERT INTO users
+          SET comp_id = :comp_id,
+              stu_name = :stu_name,
+              phone_number = :phone_number,
+              passwd = :passwd,
+              school_year = :school_year,
+              major = :major,
+              bio = :bio";
 	try {
-    	$statement = $db->prepare($query);
-		$statement->bindValue(':comp_id', $comp_id); 
-		$statement->bindValue(':name', $name); 
-		$statement->bindValue(':phone_number', $phone_number); 
-		$statement->bindValue(':passwd', $passwd);
-		$statement->bindValue(':school_year', $school_year);
-		$statement->bindValue(':major', $major);
-		$statement->bindValue(':bio', $bio);
-    	$statement->execute();
-    	$statement->closeCursor();
+		$statement = $db->prepare($query);
+        $statement->bindValue(':comp_id', $comp_id); 
+        $statement->bindValue(':stu_name', $stu_name); 
+        $statement->bindValue(':phone_number', $phone_number); 
+        $statement->bindValue(':passwd', $passwd);
+        $statement->bindValue(':school_year', $school_year);
+        $statement->bindValue(':major', $major);
+        $statement->bindValue(':bio', $bio);
+        $statement->execute();
+        $statement->closeCursor();
 	}
 	catch (PDOException $e) {
 		echo $e->getMessage();
 	}
 	catch (Exception $e) {
-		echo $e->getMessage();
+		echo $e->getMessage(); 
 	}
 
+}
+
+function getAllUsers()
+{
+    global $db;
+	$query = "SELECT * FROM users";
+	$statement = $db->prepare($query);
+	$statement->execute();
+	$results = $statement->fetchAll(); // fetch() gets only onw row; fetchAll() gets all rows
+	$statement->closeCursor();
+	return $results;
 }
 ?>
