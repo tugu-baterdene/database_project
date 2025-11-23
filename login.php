0f35d2391d5c0a7cdcbfea07c04b22d2c279d53f
gracej00sung <rgp3qv@virginia.edu>
2025-11-23 18:40:15 -0500

    added login page with design

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Roommate Connections – Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+
+    <div class="header">Roommate Connections</div>
+
+    <div class="login-wrapper">
+        <h1 class="login-title">LOG IN</h1>
+
+        <p class="signup-line">
+            Don't have an account?
+            <a href="sign_up.php">Sign up</a>
+        </p>
+
+        <form class="login-form" method="POST">
+            <label>Computing ID</label>
+            <input type="text" name="computing_id">
+
+            <label>Password</label>
+            <input type="password" name="password">
+        </form>
+    </div>
+
+</body>
+</html>
