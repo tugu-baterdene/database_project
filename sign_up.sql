055cbc9a41f02809f124105075dc1b38dfa998a9
gracej00sung <rgp3qv@virginia.edu>
2025-11-30 19:51:02 -0500

    added preferences to .sql file

--- sign_up.sql
+++ sign_up.sql
@@ -8,3 +8,26 @@ CREATE TABLE users (
     bio VARCHAR(300),
     status VARCHAR(20) DEFAULT 'searching',
     PRIMARY KEY (comp_id));
+
+CREATE TABLE preferences (
+    comp_id VARCHAR(10) NOT NULL,
+    on_off_grounds VARCHAR(15),
+    sleeping VARCHAR(50),
+    num_of_roommates INT,
+    drinking VARCHAR(20),
+    smoking VARCHAR(20),
+    pets VARCHAR(50),
+    budget DECIMAL(10,2),
+    CONSTRAINT check_pref_on_off
+        CHECK (on_off_grounds IN ('On', 'Off')),
+    CONSTRAINT check_drinking
+        CHECK (drinking IN ('Yes', 'No')),
+    CONSTRAINT check_smoking
+        CHECK (smoking IN ('Yes', 'No')),
+    CONSTRAINT check_pets
+        CHECK (pets IN ('Yes', 'No')),
+    PRIMARY KEY (comp_id),
+    FOREIGN KEY (comp_id) REFERENCES users(comp_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
