62dd989996378f276cae71b1e5946eba5bdd3605
gracej00sung <rgp3qv@virginia.edu>
2025-11-30 19:52:06 -0500

    changed tabs on header

--- header.php
+++ header.php
@@ -9,38 +9,14 @@
       </button>
       <div class="collapse navbar-collapse" id="collapsibleNavbar">
         <ul class="navbar-nav ms-auto">
-          <!-- check if currently logged in, display Log out button 
-               otherwise, display sign up and log in buttons -->
-          <?php if (!isset($_SESSION['username'])) { ?>              
-            <li class="nav-item">
-              <a class="nav-link" href="register.php">Join our community</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="signin.php">Sign in</a>
-            </li>              
-          <?php  } else { ?>                    
-            <li class="nav-item">                  
-              <a class="nav-link" href="signout.php">Sign out</a>
-            </li>
-          <?php } ?>
-        
           <li class="nav-item">
-            <a class="nav-link" href="#">Software</a>
-          </li>            
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" role="button" data-bs-toggle="dropdown" aria-expanded="false">Research</a>
-            <ul class="dropdown-menu" aria-labelledby="dropdown01">
-              <li><a class="dropdown-item" href="#">Design</a></li>
-              <li><a class="dropdown-item" href="#">Development</a></li>
-              <li><a class="dropdown-item" href="#">Testing</a></li>
-              <li><a class="dropdown-item" href="#">Maintenance</a></li>
-            </ul>
+            <a class="nav-link" href="search.php">Search</a>
+          </li>
+		  <li class="nav-item">
+            <a class="nav-link" href="search.php">My Roommates</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Activity</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" href="profile.php">My Profile</a>
           </li>
         </ul>
       </div>
