1ace6cf8ffe3f9571d82e8ac0f350fa43ae2904b
gracej00sung <rgp3qv@virginia.edu>
2025-12-07 01:57:15 -0500

    update user's passwd with hash

--- profile.php
+++ profile.php
@@ -11,12 +11,6 @@ if (!isset($_SESSION['user_id'])) {
 
 $user_id = $_SESSION['user_id'];
 $user = fetchUser($user_id);
-
-if (!$user) {
-    echo "<p>Error: user not found.</p>";
-    exit();
-}
-
 $pref = fetchPref($user_id);
 ?>
 
@@ -32,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 		updateUser($user_id, $_POST['stu_name'], $_POST['phone_number'], $_POST['school_year'], $_POST['major'], $_POST['bio']);
 		$user = fetchUser($user_id);
 	}
-    if (!empty($_POST['savePref'])) 
+    if (!empty($_POST['savePref']))
     {
         updatePref($user_id, $_POST['on_off'], $_POST['sleep'], $_POST['num_roommates'], $_POST['drinking'], $_POST['smoking'], $_POST['pets'], $_POST['budget']);
         $pref = fetchPref($user_id);
@@ -110,13 +104,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                             Preferences
                         </button>
                     </li>
-
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="tab3-tab" data-bs-toggle="tab"
-                                data-bs-target="#tab3" type="button" role="tab">
-                            Settings(?)
-                        </button>
-                    </li>
                 </ul>
 
                 <!-- TAB CONTENT -->
@@ -130,12 +117,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                 <div class="col-md-6">
                                     <label>Name</label>
                                     <input type="text" class="form-control" id='stu_name' name='stu_name'
-										value="<?php if ($user['stu_name'] !=null) echo $user['stu_name']; ?>" />
+										value="<?php if ($user['stu_name'] !=null) echo $user['stu_name']; ?>" required/>
                                 </div>
                                 <div class="col-md-6">
                                     <label>Phone Number</label>
                                     <input type="text" class="form-control" id='phone_number' name='phone_number'
-										value="<?php if ($user['phone_number'] !=null) echo $user['phone_number']; ?>" />
+										value="<?php if ($user['phone_number'] !=null) echo $user['phone_number']; ?>" required/>
                                 </div>
                             </div>
 
@@ -144,11 +131,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                 <div class="col-md-6">
                                     <label>Major</label>
                                     <input type="text" class="form-control" id='major' name='major'
-										value="<?php if ($user['major'] !=null) echo $user['major']; ?>" />
+										value="<?php if ($user['major'] !=null) echo $user['major']; ?>" required/>
                                 </div>
                                 <div class="col-md-6">
                                     <label>School Year</label>
-                                    <select class="form-control" id='school_year' name='school_year'>
+                                    <select class="form-control" id='school_year' name='school_year' required>
 										<option selected></option>
 										<option value='1'
 											<?php if ($user['school_year'] == '1') 
@@ -189,12 +176,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                 <div class="col-md-12">
                                     <label>Bio</label>
                                     <input type="text" class="form-control" id='bio' name='bio'
-										value="<?php if ($user['bio'] !=null) echo $user['bio']; ?>" />
+										value="<?php if ($user['bio'] !=null) echo $user['bio']; ?>" required/>
                                 </div>
                             </div>
 
                             <div class="text-end">
-								<input type="submit" class="btn btn-primary" value="SAVE" id="saveUser" name="saveUser" />  
+								<input type="submit" class="btn btn-primary" value="SAVE" id="saveUser" name="saveUser"/>  
                             </div>
 
                         </form>
@@ -207,7 +194,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 							<div class="row mb-3">
                                 <div class="col-md-6">
                                     <label>On/Off Grounds</label>
-                                    <select class="form-control" id='on_off' name='on_off'>
+                                    <select class="form-control" id='on_off' name='on_off' required>
 										<option selected></option>
 										<option value='On'
 											<?php if ($pref['on_off_grounds'] == 'On') 
@@ -223,7 +210,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                 </div>
                                 <div class="col-md-6">
                                     <label>Pets</label>
-                                    <select class="form-control" id='pets' name='pets'>
+                                    <select class="form-control" id='pets' name='pets' required>
 										<option selected></option>
 										<option value='Yes'
 											<?php if ($pref['pets'] == 'Yes') 
@@ -241,7 +228,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 							<div class="row mb-3">
                                 <div class="col-md-6">
                                     <label>Drinking</label>
-                                    <select class="form-control" id='drinking' name='drinking'>
+                                    <select class="form-control" id='drinking' name='drinking' required>
 										<option selected></option>
 										<option value='Yes'
 											<?php if ($pref['drinking'] == 'Yes') 
@@ -257,7 +244,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                 </div>
                                 <div class="col-md-6">
                                     <label>Smoking</label>
-                                    <select class="form-control" id='smoking' name='smoking'>
+                                    <select class="form-control" id='smoking' name='smoking' required>
 										<option selected></option>
 										<option value='Yes'
 											<?php if ($pref['smoking'] == 'Yes') 
@@ -276,19 +263,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                 <div class="col-md-6">
                                     <label>Desired Number of Roommates</label>
                                     <input type="text" class="form-control" id='num_roommates' name='num_roommates'
-										value="<?php if ($pref['num_of_roommates'] !=null) echo $pref['num_of_roommates']; ?>" />
+										value="<?php if ($pref['num_of_roommates'] !=null) echo $pref['num_of_roommates']; ?>" required/>
                                 </div>
                                 <div class="col-md-6">
                                     <label>Budget</label>
                                     <input type="text" class="form-control" id='budget' name='budget'
-										value="<?php if ($pref['budget'] !=null) echo $pref['budget']; ?>" />
+										value="<?php if ($pref['budget'] !=null) echo $pref['budget']; ?>" required />
                                 </div>
                             </div>
 							<div class="row mb-3">
                                 <div class="col-md-12">
                                     <label>Sleeping Habits/Preferences</label>
                                     <input type="text" class="form-control" id='sleep' name='sleep'
-										value="<?php if ($pref['sleeping'] !=null) echo $pref['sleeping']; ?>" />
+										value="<?php if ($pref['sleeping'] !=null) echo $pref['sleeping']; ?>" required/>
                                 </div>
                             </div>
 
@@ -297,13 +284,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                             </div>
 						</form>
                     </div>
-
-                    <!-- TAB 3 CONTENT -->
-                    <div class="tab-pane fade" id="tab3" role="tabpanel">
-                        <h4>Settings(?)</h4>
-                        <p>This is where setting information goes(?)</p>
-                    </div>
-
                 </div>
 
             </div>
